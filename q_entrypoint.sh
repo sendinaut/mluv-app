14cc898d33e09c4dc4e8ce18d1da0edcec8f7dd6
Dmytro <ys33ys33ys55@gmail.com>
2026-05-30 10:21:39 +0200

    smalle changes

---
+++ q_entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+echo "Waiting for database..."
+uv run manage.py wait_for_db
+
+echo "Starting worker..."
+exec uv run manage.py qqcluste
---
+++ web_entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+echo "Waiting for database..."
+uv run python manage.py wait_for_db
+
+echo "Running migrations..."
+uv run python manage.py makemigrations --noinput
+uv run python manage.py migrate --noinput
+
+echo "Collecting static files..."
+uv run python manage.py collectstatic --noinput
+
+echo "Starting Gunicorn..."
+exec uv run gunicorn crm_for_yasya.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers 1 --threads 3
