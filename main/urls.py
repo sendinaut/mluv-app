0a6bbd5602d6358f820694fbb33b2b7e16131dfe
Dmytro <ys33ys33ys55@gmail.com>
2026-05-31 19:14:41 +0200

    small changes

--- main/urls.py
+++ main/urls.py
@@ -10,5 +10,9 @@ app_name = "main"
 urlpatterns = [
     path("", menu_view, name="index"),
     path("menu/", menu_view, name="menu"),
-    path("publichna-oferta", TemplateView.as_view(template_name="main/public_offer.html"), name="public-offer-url"),
+    path(
+        "publichna-oferta",
+        TemplateView.as_view(template_name="main/public_offer.html"),
+        name="public-offer-url",
+    ),
 ]
