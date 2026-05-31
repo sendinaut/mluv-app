be8c16b396bf59d0862f681aebeb9f9bea5c5197
Dmytro <ys33ys33ys55@gmail.com>
2026-05-31 19:14:25 +0200

    add public offer

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from main.views import (
     menu_view,
@@ -9,4 +10,5 @@ app_name = "main"
 urlpatterns = [
     path("", menu_view, name="index"),
     path("menu/", menu_view, name="menu"),
+    path("publichna-oferta", TemplateView.as_view(template_name="main/public_offer.html"), name="public-offer-url"),
 ]
